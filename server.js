b23733bc903a254679104ec0f36d3d9b62bd8ede
atnth <atnpyth@gmail.com>
2024-07-23 15:46:24 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; // Define port with a default value
 
 // Middleware to log IP addresses
 app.use((req, _res, next) => {
-    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    const ip = req.headers['x-forwarded-for']?.split(',').shift() || req.connection.remoteAddress;
     console.log('Visitor IP:', ip);
     next();
 });
 
 // Serve static files from the public directory
-app.use(express.static("./public"));    
+app.use(express.static(path.join(__dirname, 'public')));    
 
 app.listen(port, () => {
     console.log(`Server is running at https://atnth.github.io/hoidap247/`);
